18698451a97b7c2e72dee972a3385926f7b0e704
harrisoncrist <ahcrist21@gmail.com>
2025-04-19 23:42:42 -0400

    added chloropleth and format
    still need to make accurate buckets for the chloropleth

--- scripts.js
+++ scripts.js
@@ -3,9 +3,13 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWhjcmlzdCIsImEiOiJjbTkyMGNvYTkwMHM2MmxuM2ZveGE0cHMyIn0.cETUTrGOPhzETUiIkdsXdg';
 const map_right = new mapboxgl.Map({
 	container: 'map_right', // container ID
-	style: 'mapbox://styles/mapbox/streets-v12', // style URL
+	style: 'mapbox://styles/mapbox/light-v11', // Light style
 	center: [-73.99530, 40.74676], // starting position [lng, lat]
 	zoom: 8, // starting zoom
+	maxBounds: [
+		[-74.65801, 40.48017],
+		[-73.46734, 41.08823]
+	] // max bounds
 });
 
 //load the map
@@ -17,14 +21,21 @@ map_right.on('load', () => {
 		data: './hdma_ct_boundaries_nyc.geojson'
 	})
 
-map_right.addLayer({
-	id: 'ct_boundaries',
-	type: 'fill',
-	source: 'ct_boundaries',
-	paint: {
-		'fill-color': '#088'
-	} 
+	map_right.addLayer({
+		id: 'loan_value_chloro',
+		type: 'fill',
+		source: 'ct_boundaries',
+		paint: {
+			'fill-color': ['interpolate', ['linear'], ['to-number', ['get', 'hmda_join_2018_2023_mean_loan_ct_2023']], //mean loan amount by ct for 2023
+				0, '#ff0000',     // Red
+				25, '#ff8000',    // Orange
+				50, '#ffff00',    // Yellow
+				75, '#80ff00',    // Yellow-Green
+				100, '#00ff00'],  // Green
+				'fill-opacity': 0.6,
+				'fill-outline-color': '#000000'
+		}
 
-})
+	})
 
 })
