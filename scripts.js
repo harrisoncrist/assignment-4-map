644142790180711ed28d00bca80140d26ec56667
harrisoncrist <ahcrist21@gmail.com>
2025-04-20 14:32:58 -0400

    updated map
    now includes buttons with 2018 and 2023 data

--- scripts.js
+++ scripts.js
@@ -21,21 +21,56 @@ map_right.on('load', () => {
 		data: './hdma_ct_boundaries_nyc.geojson'
 	})
 
+	//add 2023 layer source
 	map_right.addLayer({
-		id: 'loan_value_chloro',
+		id: 'loan_value_chloro_2023',
 		type: 'fill',
 		source: 'ct_boundaries',
 		paint: {
-			'fill-color': ['interpolate', ['linear'], ['to-number', ['get', 'hmda_join_2018_2023_mean_loan_ct_2023']], //mean loan amount by ct for 2023
+			'fill-color': ['interpolate', ['linear'], ['to-number', ['get', 'hmda_join_2018_2023_mean_loan_ct_2023']], //convert mean loan amount to a number by ct for 2023
 				0, '#ff0000',     // Red
 				25, '#ff8000',    // Orange
 				50, '#ffff00',    // Yellow
 				75, '#80ff00',    // Yellow-Green
 				100, '#00ff00'],  // Green
-				'fill-opacity': 0.6,
-				'fill-outline-color': '#000000'
+			'fill-opacity': 0.6,
+			'fill-outline-color': '#000000'
+		},
+		layout: {
+			visibility: 'none'
 		}
+	});
 
-	})
+	//add 2018 layer 
+	map_right.addLayer({
+		id: 'loan_value_chloro_2018',
+		type: 'fill',
+		source: 'ct_boundaries',
+		paint: {
+			'fill-color': ['interpolate', ['linear'], ['to-number', ['get', 'hmda_join_2018_2023_mean_loan_ct_2018']], //convert mean loan amount to a number by ct for 2018
+				0, '#ff0000',     // Red
+				25, '#ff8000',    // Orange
+				50, '#ffff00',    // Yellow
+				75, '#80ff00',    // Yellow-Green
+				100, '#00ff00'],  // Green
+			'fill-opacity': 0.6,
+			'fill-outline-color': '#000000'
+		},
+		layout: {
+			visibility: 'none'
+		}
+	});
 
+	document.getElementById('btn-loan-2018').addEventListener('click', () => showLayer('loan_value_chloro_2018'));
+	document.getElementById('btn-loan-2023').addEventListener('click', () => showLayer('loan_value_chloro_2023'));
+
+	function showLayer(layerShow) {
+		const layers = ['loan_value_chloro_2023', 'loan_value_chloro_2018'];
+		layers.forEach(layer => {
+			const visibility = (layer === layerShow) ? 'visible' : 'none';
+			if (map_right.getLayer(layer)) {
+				map_right.setLayoutProperty(layer, 'visibility', visibility);
+			}
+		});
+	}
 })
