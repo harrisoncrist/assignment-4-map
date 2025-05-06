47f03ee8c018ab99f06112c1fc0665525054b8ee
harrisoncrist <ahcrist21@gmail.com>
2025-05-06 11:55:51 -0400

    two maps commit

--- scripts.js
+++ scripts.js
@@ -1,40 +1,50 @@
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWhjcmlzdCIsImEiOiJjbTkyMGNvYTkwMHM2MmxuM2ZveGE0cHMyIn0.cETUTrGOPhzETUiIkdsXdg';
-const map_right = new mapboxgl.Map({
-	container: 'map_right', // container ID
-	style: 'mapbox://styles/mapbox/light-v11', // Light style
-	center:[-73.97996, 60.69785],
-	zoom: 7,
-	maxBounds: [
-    [-74.81247, 40.42274], 
-    [-73.55013, 40.94901]
-]// max bounds
+const bounds = [
+
+	[-75.39440, 40.12631],
+	[-72.93263, 41.25213]
+];
+
+// Create map_one_frame, which is an option bw 2018 and 2023 average loan values
+
+const map_one_frame = new mapboxgl.Map({
+	container: 'map_one_frame', // container ID
+	style: 'mapbox://styles/mapbox/dark-v11', // dark style
+	cooperativeGestures: true,
+	zoom: 9,
+	center: [-73.99932, 40.71125],
+	maxBounds: bounds
 });
 
 //load the map
-map_right.on('load', () => {
+map_one_frame.on('load', () => {
 
 	//add geojson source
-	map_right.addSource('ct_boundaries', {
+	map_one_frame.addSource('ct_boundaries', {
 		type: 'geojson',
 		data: './A_hdma_acs_18_23.geojson',
 	})
+	//disable scroll zoom on map_one_frame
+	//map_one_frame.scrollZoom.disable();
+	map_one_frame.addControl(new mapboxgl.FullscreenControl());
 
 	//add 2023 layer source
-	map_right.addLayer({
+	map_one_frame.addLayer({
 		id: 'layer_2023',
 		type: 'fill',
 		source: 'ct_boundaries',
 		paint: {
 			'fill-color': ['interpolate', ['linear'], ['get', 'B_download_acs_hdma_2018_2023_average_loan_2023'],
-				0, '#ff0000',     // Red
-				25, '#ff8000',    // Orange
-				50, '#ffff00',    // Yellow
-				75, '#80ff00',    // Yellow-Green
-				100, '#00ff00'],  // Green
+				281000, '#ff0000',     // Red
+				411667, '#ff8000',    // Orange
+				535000, '#ffff00',    // Yellow
+				725000, '#80ff00',    // Yellow-Green
+				1165000, '#00ff00',  // Green
+				2005000, 'blue'],
 			'fill-opacity': 0.6,
-			'fill-outline-color': '#000000'
+			'fill-outline-color': 'rgba(0, 0, 0, 0)'
 		},
 		layout: {
 			visibility: 'none'
@@ -42,19 +52,20 @@ map_right.on('load', () => {
 	});
 
 	//add 2018 layer 
-	map_right.addLayer({
+	map_one_frame.addLayer({
 		id: 'layer_2018',
 		type: 'fill',
 		source: 'ct_boundaries',
 		paint: {
 			'fill-color': ['interpolate', ['linear'], ['get', 'B_download_acs_hdma_2018_2023_average_loan_2018'],
-				0, '#ff0000',     // Red
-				25, '#ff8000',    // Orange
-				50, '#ffff00',    // Yellow
-				75, '#80ff00',    // Yellow-Green
-				100, '#00ff00'],  // Green
+				281000, '#ff0000',     // Red
+				411667, '#ff8000',    // Orange
+				535000, '#ffff00',    // Yellow
+				725000, '#80ff00',    // Yellow-Green
+				1165000, '#00ff00',  // Green
+				2005000, 'blue'],
 			'fill-opacity': 0.6,
-			'fill-outline-color': '#000000'
+			'fill-outline-color': 'rgba(0, 0, 0, 0)'
 		},
 		layout: {
 			visibility: 'none'
@@ -79,15 +90,15 @@ map_right.on('load', () => {
 		const layers = ['layer_2023', 'layer_2018'];
 		layers.forEach(layer => {
 			const visibility = (layer === layer_to_show) ? 'visible' : 'none';
-			if (map_right.getLayer(layer)) {
-				map_right.setLayoutProperty(layer, 'visibility', visibility);
+			if (map_one_frame.getLayer(layer)) {
+				map_one_frame.setLayoutProperty(layer, 'visibility', visibility);
 			}
 		});
 	}
 })
 
-map_right.on('click', ['layer_2023', 'layer_2018'], (e) => {
-	const features = map_right.queryRenderedFeatures(e.point, {
+map_one_frame.on('click', ['layer_2023', 'layer_2018'], (e) => {
+	const features = map_one_frame.queryRenderedFeatures(e.point, {
 		layers: ['layer_2023', 'layer_2018']
 	});
 
@@ -99,4 +110,61 @@ map_right.on('click', ['layer_2023', 'layer_2018'], (e) => {
 	document.getElementById('avg-loan-2023').textContent = props.B_download_acs_hdma_2018_2023_average_loan_2023 ? `$${Number(props.B_download_acs_hdma_2018_2023_average_loan_2023).toLocaleString()}` : 'N/A';
 	/*document.getElementById('avg-property').textContent = props.avg_property_value ? `$${Number(props.avg_property_value).toLocaleString()}` : 'N/A';*/
 	document.getElementById('info-box').classList.remove('hidden');
+});
+
+
+// Create map_two_frame, which is the difference between 2023 and 2018 average loan values
+const map_two_frame = new mapboxgl.Map({
+	container: 'map_two_frame', // container ID
+	style: 'mapbox://styles/mapbox/dark-v11', // dark style
+	cooperativeGestures: true,
+	zoom: 9,
+	pitch: 60,
+	bearing: -40,
+	center: [-73.99932, 40.71125],
+	maxBounds: bounds
+});
+
+map_two_frame.on('load', () => {
+	//modify geojson to add difference in loan value property
+	fetch('./A_hdma_acs_18_23.geojson')
+		.then(response => response.json())
+		.then(modified_hdma_data => {
+			modified_hdma_data.features.forEach(feature => {
+				const avgLoan2018 = feature.properties.B_download_acs_hdma_2018_2023_average_loan_2018;
+				const avgLoan2023 = feature.properties.B_download_acs_hdma_2018_2023_average_loan_2023;
+				// Check if both properties exist before calculating the difference
+				if (
+					avgLoan2018 != null &&
+					avgLoan2023 != null &&
+					!isNaN(avgLoan2018) &&
+					!isNaN(avgLoan2023)
+				) {
+					feature.properties.difference = avgLoan2023 - avgLoan2018;
+				}
+			});
+
+			//add the layer as a source to map_two_frame
+			map_two_frame.addSource('loan_value_difference_source', {
+				type: 'geojson',
+				data: modified_hdma_data
+			});
+
+
+			//add extruded layer 
+			map_two_frame.addLayer({
+				id: 'loan_value_difference_layer',
+				type: 'fill-extrusion',
+				source: 'loan_value_difference_source',
+				filter: ['has', 'difference'],
+				paint: {
+					'fill-extrusion-height': ['*', ['get', 'difference'], 0.005], // scaled height
+					'fill-extrusion-base': 0,
+					'fill-extrusion-color': 'red',
+					'fill-extrusion-opacity': 0.6,
+				}
+			});
+
+		});
+//add a info panel??
 });
